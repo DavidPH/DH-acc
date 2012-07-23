ec29807813ad5399036baeacb0eb64d5c357d855
DavidPH <DavidPH55@verizon.net>
2012-07-23 00:00:16 -0400

    Stole DS's number parser for get_bigsint. This should make the preprocessor's numbers more correct.

--- src/bignum.cpp
+++ src/bignum.cpp
@@ -1,6 +1,6 @@
 //-----------------------------------------------------------------------------
 //
-// Copyright(C) 2011 David Hill
+// Copyright(C) 2011-2012 David Hill
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -23,7 +23,33 @@
 
 #include "bignum.hpp"
 
+#include "SourceException.hpp"
+
 #include <sstream>
+
+
+//----------------------------------------------------------------------------|
+// Static Functions                                                           |
+//
+
+static inline int char_to_int(char c)
+{
+   switch(c)
+   {
+   case '0': case '1': case '2': case '3': case '4':
+   case '5': case '6': case '7': case '8': case '9':
+      return c - '0';
+
+   case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
+      return c - 'A' + 10;
+
+   case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
+      return c - 'a' + 10;
+
+   default:
+      return -1;
+   }
+}
 
 
 //----------------------------------------------------------------------------|
@@ -44,12 +70,52 @@ bigreal get_bigreal(std::string const &value, SourcePosition const &)
 //
 // get_bigsint
 //
-bigsint get_bigsint(std::string const &value, SourcePosition const &)
+bigsint get_bigsint(std::string const &value, SourcePosition const &pos)
 {
-   bigsint i;
-   std::istringstream iss(value);
-   iss >> i;
-   return i;
+   int         base = 10;
+   char const *str  = value.c_str();
+   biguint     val  = 0;
+
+   if(*str == '0')
+   {
+      ++str;
+
+      if(*str == 'X' || *str == 'x')
+      {
+         base = 16;
+         ++str;
+      }
+      else if(*str == 'B' || *str == 'b')
+      {
+         base = 2;
+         ++str;
+      }
+      else
+         base = 8;
+   }
+
+   for(char c; (c = *str) != '\0'; ++str)
+   {
+      int i = char_to_int(c);
+
+      if(i > base)
+         ERROR_P("character out of range");
+
+      if(i < 0)
+      {
+         if(c == 'U' || c == 'u' || // unsigned
+            c == 'L' || c == 'l' || // long
+            c == 'H' || c == 'h')   // short
+            break;
+
+         ERROR_P("character out of range");
+      }
+
+      val *= base;
+      val += i;
+   }
+
+   return val;
 }
 
 // EOF
